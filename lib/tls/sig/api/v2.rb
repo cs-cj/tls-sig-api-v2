7b1d60e824dc6c758b615abcfd77c2c57ad32228
lewis <lewislinlin@gmail.com>
2019-08-23 20:59:03 +0800

    fix deflate

--- lib/tls/sig/api/v2.rb
+++ lib/tls/sig/api/v2.rb
@@ -4,6 +4,7 @@ require 'json'
 # require 'rest-client'
 require 'base64'
 require 'openssl'
+require "zlib"
 require 'digest/md5'
 require 'uri'
 require 'time'
@@ -43,15 +44,15 @@ module Tls
             currTime = Time.now.to_i
             sigDoc = {
               "TLS.ver": "2.0",
-              "TLS.identifier": identifier.to_s,
-              "TLS.sdkappid": sdkappid.to_s,
-              "TLS.expire": expire.to_s,
-              "TLS.time": currTime.to_s
+              "TLS.identifier": identifier,
+              "TLS.sdkappid": sdkappid ,
+              "TLS.expire": expire ,
+              "TLS.time": currTime
             }
             base64UserBuf = nil
             if userbuf != nil 
               # base64UserBuf = new BASE64Encoder().encode(userbuf);
-              base64UserBuf = Base64.encode64(userbuf)
+              base64UserBuf = Base64.encode64(userbuf) #.gsub("\n","")
               sigDoc["TLS.userbuf"] = base64UserBuf
             end
             sig = hmacsha256(identifier, currTime, expire, base64UserBuf);
@@ -61,31 +62,33 @@ module Tls
             
             sigDoc["TLS.sig"] = sig
             
-            # todo
+            # todo node js to ruby
             # var compressed = zlib.deflateSync(newBuffer(JSON.stringify(sigDoc))).toString('base64');
             # return base64url.escape(compressed);
-
-            compressed = Base64.encode64(sigDoc.to_json)
-            URI.escape(compressed)
+            puts sigDoc
+            json = sigDoc.to_json
+            puts json
+            data_compressed = Zlib::Deflate.deflate(json)
+            base64str = Base64.encode64(data_compressed) #.gsub("\n","")
+            URI.escape(base64str)
           end
 
           protected
 
           def hmacsha256(  identifier,   currTime,   expire,   base64Userbuf)
-            contentToBeSigned = "TLS.identifier:" + identifier.to_s + "\n"
-               + "TLS.sdkappid:" + sdkappid.to_s + "\n"
-               + "TLS.time:" + currTime.to_s + "\n"
-               + "TLS.expire:" + expire.to_s + "\n";
+            contentToBeSigned = "TLS.identifier:#{identifier}\n" + "TLS.sdkappid:#{sdkappid}\n" + "TLS.time:#{currTime}\n" + "TLS.expire:#{expire}\n";
            if base64Userbuf != nil 
                contentToBeSigned += "TLS.userbuf:" + base64Userbuf + "\n";
            end
 
-           key = "key"
-           contentToBeSigned = "aaa"
-           byteKey = key.encode('utf-8')
-           data = contentToBeSigned.encode('utf-8')
-           res = OpenSSL::HMAC.digest("sha256", byteKey, data)
-           res = Base64.encode64(res)
+          #  key = "key"
+          #  contentToBeSigned = "aaa"
+          #  byteKey = key.encode('utf-8')
+          #  data = contentToBeSigned.encode('utf-8')
+            puts key
+            puts contentToBeSigned
+           res = OpenSSL::HMAC.digest("sha256", key, contentToBeSigned)
+           res = Base64.encode64(res) #.gsub("\n","")
            res
          end
 
